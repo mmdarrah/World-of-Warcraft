ab5edb10e66e10131e38a5b91f3fd7ab99569335
Hamade <mmdarrah@hotmail.com>
2019-09-04 19:32:54 +0200

    Responsive Navigation Bar

--- _site/assets/javascript/app.js
+++ _site/assets/javascript/app.js
@@ -1,10 +1,12 @@
-// document.addEventListener("DOMContentLoaded", () => {
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
 
-//     anime({
-//         targets: '.stat',
-//         value: [0, 1000],
-//         round: 1,
-//         easing: 'easeInOutExpo'
-//       });
+    burger.addEventListener('click', () =>{
+        nav.classList.toggle('nav-active');
+        // alert("test")
+    });
+}
 
-// });
+navSlide();
+
